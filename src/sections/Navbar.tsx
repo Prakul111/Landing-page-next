0514c94a9b514b9260fe7d186c7c46c3a387fe5a
@Prakul111 <iamprakul111@gmail.com>
2025-07-29 19:43:28 +0530

    new commit

---
+++ src/sections/Navbar.tsx
@@ -0,0 +1,16 @@
+import { section } from "framer-motion/client";
+
+const navLinks = [
+    { label: "Home", href: "#" },
+    { label: "Features", href: "#features" },
+    { label: "Integrations", href: "#integrations" },
+    { label: "FAQs", href: "#faqs" },
+];
+
+export default function Navbar() {
+    return <section>
+        <div className="container">
+            
+        </div>
+    </section>
+}
