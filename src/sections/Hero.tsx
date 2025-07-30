51c413c31b314dc780a2a9517a68621faa7070af
@Prakul111 <iamprakul111@gmail.com>
2025-07-30 23:51:18 +0530

    navbar done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,33 @@
+import Button from "@/components/Button"
+
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24">
+        <div className="container">
+            <div className="flex justify-center">
+                 <div className="inline-flex 
+                                 py-1 px-3 
+                                 bg-gradient-to-r from-purple-400 to-pink-400
+                                 rounded-full
+                                 text-neutral-950 
+                                 font-semibold">
+                                ✨ 7.5 Million Seed Round Raised</div>
+                  </div>
+        <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">Impactful Design Created Efforlessly</h1>
+        <p className="text-center text-xl text-white/50 mt-8">Design Tools Shouldn't slow you down. Layers Combines Powerful features with an intuitive interface
+            that keep you in creative flow. </p>
+            <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                <input type="email" placeholder="Enter your email"
+                className="bg-transparent px-2 md:flex-1"/>
+                <Button type="submit" 
+                variant="primary"
+                className="whitespace-nowrap"
+                size="sm"
+                >Sign Up</Button>
+            </form>
+        </div>
+    </section>
+    
+        )
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,4 +1,7 @@
-import { section } from "framer-motion/client";
+import Image from "next/image";
+import logoImage from "@/assets/images/logo.svg";
+import Button from "@/components/Button";
+
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -8,9 +11,45 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-    return <section>
-        <div className="container">
-            
-        </div>
-    </section>
+    return (
+        <section className="py-4 lg:py-4">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3
+                border border-white/15 
+                rounded-full 
+                p-2 px-4 md:pr-2
+                items-center" >
+                    <div>
+                        <Image src={logoImage}
+                            alt="Layers Logo"
+                            className="h-9 w-auto md:h-auto" />
+                    </div>
+                    <div className="lg:flex justify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map(link => (
+                                <a href={link.href} key={link.label}>{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+
+                    <div className="flex justify-end gap-4">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="prefix__feather prefix__feather-menu md:hidden">
+                            <path d="M3 12h18M3 6h18M3 18h18" />
+                        </svg>
+                        <Button className="hidden md:inline-flex items-center" variant="secondary" >Log In</Button>
+                        <Button className="hidden md:inline-flex items-center" variant="primary">Sign Up</Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+
 }
