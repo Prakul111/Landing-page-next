ae1dfcc1c2b9ad406c281cdf323ba58498b492e9
@Prakul111 <iamprakul111@gmail.com>
2025-07-31 01:21:06 +0530

    logoticker done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,34 @@
 import Button from "@/components/Button"
+import Image from "next/image"
+import designExample1Image from "@/assets/images/design-example-1.png"
+import designExample2Image from "@/assets/images/design-example-2.png"
+import Pointer from "@/components/Pointer"
 
 
 export default function Hero() {
     return (
-        <section className="py-24">
-        <div className="container">
+        <section className="py-24 overflow-x-clip">
+        <div className="container relative">
+            <div className="absolute -left-32 top-16 hidden lg:block">
+                <Image 
+                src={designExample1Image}
+                alt="Design example 1 image" 
+                />
+            </div>
+            <div className="absolute -right-64 -top-16 hidden lg:block">
+                <Image 
+                src={designExample2Image} 
+                alt="Design example 2 image"
+                />
+            </div>
+            <div className="absolute left-56 top-96 hidden lg:block">
+                <div>
+                <Pointer name="Andrea"/>
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                <Pointer name="Bryan" color="red"/>
+                </div>
+            </div>
             <div className="flex justify-center">
                  <div className="inline-flex 
                                  py-1 px-3 
@@ -19,7 +43,7 @@ export default function Hero() {
             that keep you in creative flow. </p>
             <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
                 <input type="email" placeholder="Enter your email"
-                className="bg-transparent px-2 md:flex-1"/>
+                className="bg-transparent px-2 md:flex-1 w-full"/>
                 <Button type="submit" 
                 variant="primary"
                 className="whitespace-nowrap"
