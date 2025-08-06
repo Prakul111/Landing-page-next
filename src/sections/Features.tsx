edf4adfe38c5dd6fb7de6dd3134d330b74bdd23a
@Prakul111 <iamprakul111@gmail.com>
2025-08-07 01:03:31 +0530

    integrations

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,9 +3,10 @@ import Tag from "@/components/Tag";
 import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg"
 import avatar2 from "@/assets/images/avatar-lula-meyers.jpg"
 import avatar3 from "@/assets/images/avatar-florence-shaw.jpg"
-import { div } from "framer-motion/client";
 import Image from "next/image";
 import Avatar from "@/components/Avatar";
+import Key from "@/components/Key";
+
 
 const features = [
     "Asset Library",
@@ -24,11 +25,13 @@ export default function Features() {
                 <div className="flex justify-center">
                 <Tag>Features</Tag>
                 </div>
-            <h2 className="text-6xl font-medium text-center mt-6">Where power meets <span className="text-lime-400">simplicity</span></h2>
-            <div className="mt-12 grid grid-cols-1 gap-8">
+            <h2 className="text-6xl font-medium text-center mt-6 max-w-2xl mx-auto">Where power meets <span className="text-lime-400">simplicity</span></h2>
+            <div className="mt-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-8">
                 <FeatureCard 
                 title="Real-Time Collaboration"
-                description="Work together seamlessly with coflict-fee team editing">
+                description="Work together seamlessly with coflict-fee team editing"
+                className="md:col-span-2 lg:col-span-1"
+                >
                     <div className="aspect-video flex items-center justify-center">
                         <Avatar className="z-40">
                             <Image 
@@ -62,7 +65,9 @@ export default function Features() {
                 </FeatureCard>         
                 <FeatureCard 
                 title="Interactive Prototyping"
-                description="Engage your clients with prototypes that reacts to user actions">
+                description="Engage your clients with prototypes that reacts to user actions"
+                className="md:col-span-2 lg:col-span-1"
+                >
                     <div className="aspect-video flex items-center justify-center">
                         <p className="text-4xl font-extrabold text-white/20 text-center">
                         We've achieved <span className="bg-gradiant-to-r from-purple-400 to-pink-400 bg-clip-text bg-transparent">incredible</span> growth this year</p>
@@ -70,15 +75,24 @@ export default function Features() {
                 </FeatureCard>
                 <FeatureCard 
                 title="Keyboard quick actions"
-                description="Powerful commands to help you to create designs more quickly">
+                description="Powerful commands to help you to create designs more quickly"
+                className="md:col-span-2 lg:col-span-1 md:col-start-2 lg:col-start-auto "
+                >
+                    <div className="aspect-video flex items-center justify-center gap-4">
+                             <Key className="w-28">shift</Key>       
+                             <Key>alt</Key>
+                             <Key>C</Key>       
+                    </div>
                 </FeatureCard>
-                
             </div>
-            <div>
+            <div className="mt-8 flex flex-wrap gap-3 justify-center">
                 {features.map(feature => 
-                    <div key={feature}>
-                        <span></span>
-                        <span>{feature}</span>
+                    <div key={feature} 
+                    className="bg-neutral-900 border border-white/10 
+                    inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center">
+                        <span className="bg-lime-400 text-neutral-950 
+                        size-5 rounded-full inline-flex items-center justify-center text-xl">★</span>
+                        <span className="font-medium md:text-large">{feature}</span>
                     </div>
                 )}
             </div>
