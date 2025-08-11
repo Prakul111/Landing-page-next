43266fa93f9facd5559e0d4f337ffa1fe3f9781c
@Prakul111 <iamprakul111@gmail.com>
2025-08-12 04:35:25 +0530

    animations till feature cards

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import logoImage from "@/assets/images/logo.svg";
 import Button from "@/components/Button";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 
 
 const navLinks = [
@@ -11,14 +16,15 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <section className="py-4 lg:py-4">
+        <>
+        <section className="py-4 lg:py-4 fixed w-full top-0 z-50">
             <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3
-                border border-white/15 
-                rounded-full 
+                <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur">
+                <div className="grid grid-cols-2 lg:grid-cols-3 
                 p-2 px-4 md:pr-2
-                items-center" >
+                items-center ">
                     <div>
                         <Image src={logoImage}
                             alt="Layers Logo"
@@ -41,15 +47,51 @@ export default function Navbar() {
                             strokeWidth="2"
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            className="prefix__feather prefix__feather-menu md:hidden">
-                            <path d="M3 12h18M3 6h18M3 18h18" />
+                            className="prefix__feather prefix__feather-menu md:hidden"
+                            onClick={() =>setIsOpen(!isOpen)}
+                            >
+                                <line x1="3" y1="6" x2="21" y2="6" 
+                                className={twMerge("origin-left transition",isOpen && 'rotate-45 -translate-y-1')}>
+                                </line>
+                                
+                                <line x1="3" y1="12" x2="21" y2="12" 
+                                className={twMerge("transition",isOpen && 'opacity-0')}>
+
+                                </line>
+                                <line x1="3" y1="18" x2="21" y2="18" 
+                                className={twMerge("origin-left transition",isOpen && '-rotate-45 translate-y-1')}>
+                                </line>
+                            {/* <path d="M3 12h18M3 6h18M3 18h18" /> */}
                         </svg>
                         <Button className="hidden md:inline-flex items-center" variant="secondary" >Log In</Button>
                         <Button className="hidden md:inline-flex items-center" variant="primary">Sign Up</Button>
                     </div>
                 </div>
+                <AnimatePresence>
+                {isOpen && (
+                <motion.div
+                    initial={{height: 0}}
+                    animate={{height: "auto"}}
+                    exit={{height: 0}}
+                 className="overflow-hidden">
+                    <div className="flex flex-col items-center gap-4 py-4">
+                    {navLinks.map(link => (
+                        <a href={link.href} key={link.label} 
+                        className="">{link.label}</a>
+                    ))}
+                    <Button variant="secondary">Log In</Button>
+                    <Button variant="primary">Sign Up</Button>
+                    </div>
+                </motion.div>
+                )}
+                </AnimatePresence>
+                </div>
             </div>
         </section>
+        <div className="pb-[86px] md:pb-[98px] lg:pb-[130px]">
+
+        </div>
+        </>
     );
 
 }
